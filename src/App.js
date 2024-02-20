5018862d4af3952caf6dda31e3f50ac96dd42ec2
RobertSarsoza <sarsoza@gmail.com>
2024-02-20 14:30:26 -0800

    added navigation for user as suggested by PR comments

--- src/App.js
+++ src/App.js
@@ -1,15 +1,65 @@
-import React from "react"
-import Header from "./components/Header"
-import Footer from "./components/Footer"
 import "./App.css"
+import { Routes, Route } from "react-router-dom"
+import { useState } from "react"
+import ApartmentEdit from "./pages/ApartmentEdit"
+import ApartmentIndex from "./pages/ApartmentIndex"
+import ApartmentNew from "./pages/ApartmentNew"
+import ApartmentProtectedIndex from "./pages/ApartmentProtectedIndex"
+import ApartmentShow from "./pages/ApartmentShow"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Home from "./pages/Home"
+import mockApartments from "./mockApartments"
+import mockUsers from "./mockUsers"
+import NotFound from "./pages/NotFound"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
 
-function App() {
+const App = () => {
+  const [currentUser, setCurrentUser] = useState(mockUsers[0])
+  const [apartments, setApartments] = useState(mockApartments)
+
   return (
-    <div>
-      <Header />
+    <>
+      <Header currentUser={currentUser} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route
+          path="/apartmentindex"
+          element={<ApartmentIndex apartments={apartments} />}
+        />
+        {currentUser && (
+          <Route
+            path="/myapartments"
+            element={
+              <ApartmentProtectedIndex
+                apartments={apartments}
+                currentUser={currentUser}
+              />
+            }
+          />
+        )}
+        <Route
+          path="/apartmentshow/:id"
+          element={<ApartmentShow apartments={apartments} />}
+        />
+        <Route
+          path="/apartmentnew"
+          element={<ApartmentNew currentUser={currentUser} />}
+        />
+        <Route
+          path="/apartmentedit/:id"
+          element={
+            <ApartmentEdit currentUser={currentUser} apartments={apartments} />
+          }
+        />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
       <Footer />
-    </div>
-  );
+    </>
+  )
 }
 
 export default App
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,57 @@
 import React from "react"
-const Header = (props) => {
+import { Nav, NavItem } from "reactstrap"
+import { NavLink, useNavigate } from "react-router-dom"
+
+const Header = ({ current_user, logout }) => {
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    logout()
+    navigate("/")
+  }
+
   return (
-    <>
-      <header className="header">
-        <h1>Apartment App</h1>
-      </header>
-    </>
+    <Nav className="nav">
+      <NavItem>
+        <NavLink to="/apartmentindex" className="nav-link">
+          View Listings
+        </NavLink>
+      </NavItem>
+      {current_user ? (
+        <>
+          <NavItem>
+            <NavLink to="/myapartments" className="nav-link">
+              My Listings
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to="/apartmentnew" className="nav-link">
+              Create Listing
+            </NavLink>
+          </NavItem>
+          <NavItem as="li">
+            <form onSubmit={handleLogout}>
+              <button type="submit" className="nav-link btn-link">
+                Log Out
+              </button>
+            </form>
+          </NavItem>
+        </>
+      ) : (
+        <>
+          <NavItem>
+            <NavLink to="/login" className="nav-link">
+              Sign In
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to="/signup" className="nav-link">
+              Sign Up
+            </NavLink>
+          </NavItem>
+        </>
+      )}
+    </Nav>
   )
 }
 
