f3a7957f487eae181ec7054aebc5383d059ddefb
RobertSarsoza <sarsoza@gmail.com>
2024-02-20 15:11:08 -0800

    testing fix

--- src/_tests_/Header.test.js
+++ src/_tests_/Header.test.js
@@ -1,9 +1,10 @@
 import React from "react"
 import { BrowserRouter } from "react-router-dom"
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import Header from "../components/Header"
 
 describe("<Header />", () => {
+  
   it("renders without crashing", () => {
     const div = document.createElement("div")
     render(
@@ -12,5 +13,16 @@ describe("<Header />", () => {
       </BrowserRouter>,
       div
     )
+  })
+
+  it("displays the header text", () => {
+    render(
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
+    )
+
+    const headerText = screen.getByText(/Apartment App/i)
+    expect(headerText).toBeInTheDocument()
   })
 })
