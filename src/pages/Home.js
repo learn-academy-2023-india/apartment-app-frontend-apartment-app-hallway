187c8e39d9987a5953f29684eff95deddf7d6498
RobertSarsoza <sarsoza@gmail.com>
2024-02-21 10:00:37 -0800

    react home and not found functionality and testing

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,17 @@
 import React from "react"
+import { NavLink } from "react-router-dom"
+import HomeBackgroundImage from "../assets/HomeBackgroundImage.png"
 
-const Home = () => {
+const Home = ({ currentUser }) => {
   return (
-    <>
-    
-    </>
+    <div className="home-container" style={{ backgroundImage: `url(${HomeBackgroundImage})` }}>
+      <div className="welcome-message">
+        <h1>Welcome to Your Future Home!</h1>
+        <p>Embark on a seamless journey to discover your perfect residence. From the latest listings to exclusive properties, we connect you directly with the heartbeats of vibrant communities. For property managers, our platform offers a streamlined experience to showcase your apartments and engage with potential tenants. Let's find where your story begins—because finding a home means finding a place where you truly belong.</p>
+        <NavLink to="/listings" className="explore-button">Explore Listings</NavLink>
+      </div>
+    </div>
   )
-} 
+}
 
 export default Home
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,11 +1,14 @@
 import React from "react"
+import { NavLink } from "react-router-dom"
 
 const NotFound = () => {
   return (
-    <>
-    
-    </>
+    <div className="not-found-container">
+      <h1>404</h1>
+      <h3>This page is currently on a house-hunting adventure!</h3>
+      <NavLink to="/" className="home-button">Return Home</NavLink>
+    </div>
   )
-} 
+}
 
 export default NotFound
